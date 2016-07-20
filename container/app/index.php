b83fe3fca65dce1b9cbc090a6840b220c5979e4b
Agustín Gañan <agusgr@gmail.com>
2016-07-20 11:18:59 +0200

    Polaroid effect

--- container/app/index.php
+++ container/app/index.php
@@ -7,67 +7,40 @@
 <!DOCTYPE html>
 <html>
 <head profile="http://www.w3.org/2005/10/profile">
-    <link rel="icon" type="image/png" href="img/heart.png">
-    <title>M & A</title>
-    <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-      <style>
-      .carousel-inner > .item > img,
-      .carousel-inner > .item > a > img {
-          width: 70%;
-          margin: auto;
-      }
-      </style>
+  <title>M & A</title>
+  <link rel="icon" type="image/png" href="img/heart.png">
+  <link rel="stylesheet" type="text/css" href="css/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
+  <link rel="stylesheet" type="text/css" href="css/polaroid.css"/>
+  <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<div class="container">
-  <br>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-<?php
+  <script type="text/javascript" src="lib/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="lib/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="lib/slick.min.js"></script>
 
-    $class = 'active';
-    for ($x = 0; $x < 9; $x++) {
-        echo '<li data-target="#myCarousel" data-slide-to="'.  $x  . '" class="'. $class .'"></li>';
-        $class = '';
-    }
-
-?>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-
+<div id="polaroid" class="instaslider">  
 <?php
 
     $medias = Instagram::getTopMediasByTagName('wood');
-    $class = 'active';
-    foreach ($medias as $key => $media) {
-        echo '<div class="item '. $class .'">';
-        echo '<img src="'. $media->imageStandardResolutionUrl . '"width="460" height="345">';
-        echo '<div class="carousel-caption">';
-        echo '<p>'. $media->caption .'</p>';
-        echo '</div>';
-        echo '</div>';
-        $class = '';
+    foreach ($medias as $key => $media) {        
+        echo '<figure>';        
+        echo '<img src="'. $media->imageStandardResolutionUrl . '"/>';
+        echo '<figcaption>'. $media->caption . '</figcaption>';
+        echo '</figure>';      
     }
 ?>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+<script type="text/javascript">
+  $(document).ready(function(){
+     $('.instaslider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+     });    
+  });
+</script>
 </div>
 </body>
 </html>
